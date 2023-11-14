ac26e6891db0e4e4fb920f2eff403537f5cf5a59
Richard Benson <richard@rkben.dev>
2023-11-14 10:28:45 -0500

    fix: clone depth, no need to run twice to build

--- automatic-sqlite-generation.sh
+++ automatic-sqlite-generation.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+#
+CWD=`pwd`
+
+build_libretrodb_tool() {
+  cd RetroArch/libretro-db
+  make clean
+  make all
+  cp ./libretrodb_tool $CWD
+  cd $CWD
+}
+
 if [ ! -d libretro-database ];then
-  git clone https://github.com/libretro/libretro-database
+  git clone --depth=1 https://github.com/libretro/libretro-database
 else
   cd libretro-database
   git pull
-  cd ..
+  cd $CWD
 fi
 if [ ! -d RetroArch ];then
-  git clone https://github.com/libretro/RetroArch
+  git clone --depth=1 https://github.com/libretro/RetroArch
 else
   cd RetroArch/libretro-db
   git pull
-  make clean
-  make all
-  cp ./libretrodb_tool ../..
-  cd ../..
+  cd $CWD
 fi
+
+build_libretrodb_tool
+unset CWD
+rm -f libretro-db.sqlite
 python3 libretro2sqlite.py
